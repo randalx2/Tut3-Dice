b897626b5857906c03f5619327934f0a12074a6a
randalx2 <pmisthry@gmail.com>
2016-03-03 21:35:27 +0200

    Testing arrays

--- Tut3/main.cpp
+++ Tut3/main.cpp
@@ -15,26 +15,26 @@ using namespace std;
 class Dice
 {
 private:  int result;   //integer attribute to store the result of a roll
-public:	  int rolls;   //integer attribute to store the number of rolls
+//public:	  int rolls;   //integer attribute to store the number of rolls
 
 public: Dice()
 {                               // Default Constructor to set up dice
 			srand(time(NULL)); //seed the randomizer
 			result = 0;       // We won't consider a result unless the dice has been rolled
-			rolls = 0;        //initially we have no rolls unless we call the roll method
+			//rolls = 0;        //initially we have no rolls unless we call the roll method
 			
 }
 
 		Dice(int resnew, int rollsnew)   //Overloading constructor
 		{
 			result = resnew;
-			rolls = rollsnew;
+			//rolls = rollsnew;
 		}
 
 		int roll() //Method which return the result of a roll when called. Also increments roll count
 		{
 			//int resnew = 0, rollsnew = 0; //initialize variables to hold updated values
-			rolls++;                     //Each time this method is called, the value of the number of rolls is incremented
+			//rolls++;                     //Each time this method is called, the value of the number of rolls is incremented
 			result = rand() % 6 + 1;     // Value of the result is changed randomly 
 
 			return result; 			
@@ -54,18 +54,41 @@ float average(int arr[], int arraylength); // receives an integer array as well 
 
 int main()
 {
-	char userinput = 'x';
-	Dice *d1 = new Dice(); // creating a dice object with default constructor
+	int temp = 0;          //temporarily hold the result of the roll here to avoid using the randomizer by calling the roll method
+	int rolls = 0;
+	int *pointer = NULL;   // initialize array pointer
+	char userinput = 'x';   //User input variable to continue or stop program
+
+	Dice *d1 = new Dice();  // creating a dice object with default constructor
 
 	//The user should be able to keep rolling his dice unless he enters a specific character to terminate the game
 	while (userinput != 'n')
 	{
+		
 		cout << "Enter any character EXCEPT 'n' to do a dice roll. Enter 'n' to stop " << endl;
 		cin >> userinput;
-		cout << "Result is: " << d1->roll() << endl; //Display the result
-		cout << "Number of Rolls: " << d1->rolls << endl;
+		rolls++;
+		pointer = new int[rolls]; // set the array dynamically according to number of rolls each time
+		                         //dynamically allocated arrays allows an array of increasing size
+		temp = d1->roll();
+		cout << "Result is: " << temp << endl; //Display the result
+		cout << "Number of Rolls: " << rolls << endl;
+
+		 //Fill out the array with each index holding the result value of each of the rolls respectively
+		for (int counter = 0; counter < rolls; counter++)
+		{
+			*(pointer + counter) = temp; // assign the next array index value with the value stored in temp			   
+			//memory addresses in arrays are left justified so increment by one to get to next value	
+		}
+		for (int counter = 0; counter < rolls; counter++)
+		{		
+			cout << "Index " << counter + 1 << " is " << *(pointer + counter) << endl; //We already filled out the array previously
+		}
+		
+
 	}
 
+	delete pointer;
 	delete d1;  //Done using dice object so remove it from memory
 	system("PAUSE");
 	return 0;
