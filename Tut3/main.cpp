3dd0bf8aa2acb4fe1239ba34d1f0ebe0621194a2
randalx2 <pmisthry@gmail.com>
2016-03-04 02:04:01 +0200

    Dice overloaded function fully working. Array overloading function working up to 19 rolls. Break point triggered at run time.

--- Tut3/main.cpp
+++ Tut3/main.cpp
@@ -38,8 +38,8 @@ public: Dice()
 
 		int roll()                        //Method which returns the result of a roll when called.
 		{
-			result = rand() % 6 + 1;     // Value of the result is changed randomly 
-			accresult = accresult + result; //Value accumulation
+			result = rand() % 6 + 1;         // Value of the result is changed randomly 
+			accresult = accresult + result; //Value accumulation each time the roll method is called
 
 			return result; 			
 		}                               //insert a conditional loop in main program to keep rolling unless a termination character is input
@@ -60,9 +60,13 @@ int main()
 {
 	int temp = 0;          //temporarily hold the result of the roll here to avoid using the randomizer by calling the roll method
 	int rolls = 0;
-	int *pointer = NULL;   // initialize array pointer
+	int *pointer = new int[rolls];   // initialize array pointer
+	
+	                         // set the array dynamically according to number of rolls each time
+	                         //dynamically allocated arrays allows an array of increasing size
+	                        //Dynamic Memory Allocation allows you to use a variable to specify the array size
 	char userinput = 'x';   //User input variable to continue or stop program
-
+	
 	Dice *d1 = new Dice();  // creating a dice object with default constructor
 
 	//The user should be able to keep rolling his dice unless he enters a specific character to terminate the game
@@ -71,15 +75,17 @@ int main()
 		cout << "Enter any character EXCEPT 'n' to do a dice roll. Enter 'n' to stop " << endl;
 		cin >> userinput;
 		rolls++;                  //increment roll counter after every roll
-		pointer = new int[rolls]; // set the array dynamically according to number of rolls each time
-		                          //dynamically allocated arrays allows an array of increasing size
-		                          //Dynamic Memory Allocation allows you to use a variable to specify the array size
+	
 		temp = d1->roll();
 		cout << "Result: " << temp << endl;
 		cout << "Rolls: " << rolls << endl;
 		cout << "Accumulated: " << d1->accresult << endl;
-		cout << "Average: " << average(*d1, rolls) << endl;  // Function works except it doesn't return float values at this point 3 March 2015
-	}
+		cout << "Average from Dice Overloaded Function : " << average(*d1, rolls) << endl;  // Function always works successfully!
+
+		//Setting up array for passing it to overloaded function
+		pointer[rolls - 1] = temp;
+		cout << "Average from Array Overloaded Function: " << average(pointer, rolls) << endl;	//Function works successfully up to 19 rolls
+	}                                                                                           //Break point is triggered upon termination
 
 	delete pointer;
 	delete d1;  //Done using dice object so remove it from memory
@@ -89,8 +95,8 @@ int main()
 
 float average(Dice d, int rolls)
 {
-	float ave = float(d.accresult / rolls);
-
+	float ave = (float)d.accresult / (float)rolls; //Used typecasting to force floating point calculations
+ 	                                               //useful when the variables cannot be changed to float due to program conflicts
 	return ave;	
 }
 
@@ -103,7 +109,7 @@ float average(int arr[], int arraylength)
 	{
 		addresult = addresult + arr[counter];
 	}
-	ave = float(addresult / arraylength);
+	ave = (float)addresult / (float)arraylength;
 
 	return ave;
 }
